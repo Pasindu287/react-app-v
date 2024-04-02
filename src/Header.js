6a6fdd290f3b0d13866cc199f0a0fc28b03493bf
Pasindu287 <pasindumalshan46@gmail.com>
2024-04-02 16:21:57 +0530

    remove icon

--- src/Header.js
+++ src/Header.js
@@ -7,9 +7,6 @@ function Header() {
     <header className="App-main-header">
       <nav>
         <ul className="nav-links">
-          <li>
-             <img src="background-image/icon.png" alt="Varicose Veins Classifer" className="icon" />
-          </li>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
         </ul>
