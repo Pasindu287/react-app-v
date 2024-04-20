0c6faa0615ff93d24338ed0764ae5810e18d69dd
Pasindu Malshan <79415517+Pasindu287@users.noreply.github.com>
2024-04-20 19:02:26 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,11 +5,8 @@ function About() {
   return (
     <div className="About">
       <h1>About</h1>
-      
-      <ul>
-             <li><strong>What is varicose veins ? </strong></li>
-              <li> Varicose veins are swollen, twisted veins that you can see just under the surface of the skin. They usually occur in the legs but can also form in other parts of the body. Varicose veins happen when the valves in the veins that regulate blood flow direction weaken, causing blood to pool in the veins and making them enlarge. While often considered a cosmetic issue, they can cause discomfort and lead to more serious health problems. Common symptoms include aching pain, heaviness, and swelling in the legs.</li>
-      </ul>
+      <p><strong>What is varicose veins ? </strong></p>
+      <p> Varicose veins are swollen, twisted veins that you can see just under the surface of the skin. They usually occur in the legs but can also form in other parts of the body. Varicose veins happen when the valves in the veins that regulate blood flow direction weaken, causing blood to pool in the veins and making them enlarge. While often considered a cosmetic issue, they can cause discomfort and lead to more serious health problems. Common symptoms include aching pain, heaviness, and swelling in the legs.</p>
       <p>This is the about page for the Varicose Veins Classifier App and the application manily classify by  varicose veins and spider veins in legs.</p>
       <p>Also provide treatment plan for readuce it by home.</p>
     </div>
